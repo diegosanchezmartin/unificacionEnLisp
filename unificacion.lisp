e55350b23472a16fed83c47f85c14557a9616eb3
Diego <79831830+diegosanchezmartin@users.noreply.github.com>
2021-09-30 00:42:26 +0200

    Update unificacion.lisp
    El esAtomo() ahora mismo no hace nada, lo nuevo es la declaración de la función unificación(e1 e2) y la de intercambiar(e1 e2) por si acaso e1 no es átomo y e2 sí y tenemos que intercambiarlos como dice el algoritmo de la teoría (línea 1)

--- unificacion.lisp
+++ unificacion.lisp
@@ -3,7 +3,24 @@
 ; © 2021: All Rights Reserved
 
 (defun esAtomo(dato)
-  (if (atom 'dato)
-    T
-    NIL)
+  (if (eq (first dato) '?)
+    (princ "Constante introducida")
+    (princ "Variable introducida"))
+)
+
+(defun unificacion(e1 e2)
+  (if (eq (atom e1) 'T)
+    (print "e1 es atomo") ;if
+    (if (eq (atom e2) 'T) ;else
+      (intercambiar e1 e2)
+      continuar(e1 e2)
+  )
+
+  (print "")
+)
+
+(defun intercambiar(e1 e2)
+  (setq aux e1)
+  (setq e1 e2)
+  (setq e2 aux)
 )
